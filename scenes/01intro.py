d4885479b0e8bfea01e20e9785203473989c9598
ericsmoore <esmoore510@gmail.com>
2026-06-03 19:59:09 -0500

    add options to scene 01, adjust spacing for it

--- scenes/01intro.py
+++ scenes/01intro.py
@@ -5,7 +5,7 @@ import time
 def scene(en: Engine):
     time.sleep(2)
 
-    x, y = en.c.coords(22, 9)
+    x, y = en.c.coords(22, 7)
 
     en.typewrite(en.assets['lamartine'], x, y, 0.015, en.t.gray25)
     time.sleep(2)
@@ -17,12 +17,19 @@ def scene(en: Engine):
         x + 2,
         y + 11,
         0.075,
-        en.t.gray40,
+        en.t.gray50,
     )
+
+    time.sleep(1.5)
+
+    opt = 'PRESS ANY KEY'
+
+    x, y = en.c.coords(en.c.w // 2 - len(opt) // 2, en.c.h - 4)
+    en.typewrite(opt, x, y, 0.05, en.t.italic_gray30)
 
     while True:
         key = en.t.inkey().lower()
         if key == 'q':
             return None
-        if key == 'e':
-            return '01intro'
+        else:
+            return '02'
